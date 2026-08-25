be2d832bc2a95a97e696de15598c2e67cec49c84
Deva-021 <deva2402124@gmail.com>
2026-08-25 09:18:59 +0530

    Time: 2 ms (68.20%) | Memory: 45.3 MB (61.63%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
@@ -1,40 +1,20 @@
 class Solution {
     public int missingMultiple(int[] nums, int k) {
 
-        TreeSet<Integer> set = new TreeSet<>();
-        ArrayList<Integer> list = new ArrayList<>();
+        Set<Integer> hash = new HashSet<>();
 
-        // Add all elements to TreeSet
-        for (int i = 0; i < nums.length; i++) {
-            set.add(nums[i]);
+       for (int i = 0; i < nums.length; i++) {
+    hash.add(nums[i]);
+}
+
+        int multiple = k;
+
+        while(hash.contains(multiple)) {
+
+            multiple = multiple + k;
+
         }
 
-        // Store only multiples of k
-        for (int x : set) {
-            if (x % k == 0) {
-                list.add(x);
-            }
-        }
-
-        // Base condition: no multiple of k exists
-        if (list.isEmpty()) {
-            return k;
-        }
-
-        // First multiple is missing
-        if (list.get(0) != k) {
-            return k;
-        }
-
-        // Check for a missing multiple in between
-        for (int i = 0; i < list.size() - 1; i++) {
-
-            if (list.get(i + 1) - list.get(i) != k) {
-                return list.get(i) + k;
-            }
-        }
-
-        // Missing multiple is after the last element
-        return list.get(list.size() - 1) + k;
+        return multiple;
     }
 }
